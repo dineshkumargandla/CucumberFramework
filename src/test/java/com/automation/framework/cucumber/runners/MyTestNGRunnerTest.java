db36dee81b530c49d29bd29f68156523549c7298
Dinesh Kumar gandla <105041516+dineshkumargandla@users.noreply.github.com>
2022-05-13 15:28:50 +0530

    Modified extent report path

--- src/test/java/com/automation/framework/cucumber/runners/MyTestNGRunnerTest.java
+++ src/test/java/com/automation/framework/cucumber/runners/MyTestNGRunnerTest.java
@@ -6,9 +6,12 @@ import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(
-        //plugin = {"html:target/cucumber/cucumber.html","summary", "pretty"},
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        glue = {"com.automation.framework.cucumber.stepdefinations","com.automation.framework.cucumber.hooks"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "html:target/cucumber/cucumber.html",
+                "summary",
+                "pretty"},
+        glue = {"com.automation.framework.cucumber.stepdefinations",
+                "com.automation.framework.cucumber.hooks"},
         features = "src/test/java/com/automation/framework/cucumber/feature",
         monochrome = true
 )
